aae91ceec2a0d645cae8df47f8142f21ec1f1863
Lokesh Medharametla <lokeshchowdary902@gmail.com>
2024-06-28 15:09:36 +0530

    "DB Migrated"

--- db/migrate/20240628091656_add_devise_to_users.rb
+++ db/migrate/20240628091656_add_devise_to_users.rb
@@ -2,50 +2,36 @@
 
 class AddDeviseToUsers < ActiveRecord::Migration[7.1]
   def self.up
-    change_table :users do |t|
-      ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+    # Check if the column exists before adding it
+    unless column_exists?(:users, :email)
+      change_table :users do |t|
+        ## Database authenticatable
+        t.string :email, null: false, default: ""
 
-      ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+        ## Other Devise columns (e.g., encrypted_password, reset_password_token, etc.)
 
-      ## Rememberable
-      t.datetime :remember_created_at
+        ## Rememberable
+        t.datetime :remember_created_at
 
-      ## Trackable
-      # t.integer  :sign_in_count, default: 0, null: false
-      # t.datetime :current_sign_in_at
-      # t.datetime :last_sign_in_at
-      # t.string   :current_sign_in_ip
-      # t.string   :last_sign_in_ip
-
-      ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
-
-      ## Lockable
-      # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
-      # t.string   :unlock_token # Only if unlock strategy is :email or :both
-      # t.datetime :locked_at
-
-
-      # Uncomment below if timestamps were not included in your original model.
-      # t.timestamps null: false
+        ## Uncomment and modify as needed for other Devise modules
+      end
     end
 
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
-    # add_index :users, :confirmation_token,   unique: true
-    # add_index :users, :unlock_token,         unique: true
+    # Add index if the column was added
+    add_index :users, :email, unique: true unless index_exists?(:users, :email)
+
+    # Add other indexes for Devise columns like reset_password_token
+    add_index :users, :reset_password_token, unique: true unless index_exists?(:users, :reset_password_token)
   end
 
   def self.down
-    # By default, we don't want to make any assumption about how to roll back a migration when your
-    # model already existed. Please edit below which fields you would like to remove in this migration.
-    raise ActiveRecord::IrreversibleMigration
+    # Define rollback if needed
+    # For example, remove the columns and indexes added in self.up
+    if column_exists?(:users, :email)
+      remove_column :users, :email
+    end
+
+    # Add rollback for other columns and indexes as needed
+    # Example: remove_index :users, :reset_password_token
   end
 end
